5632db13e176d70ef567ad34d19fec4cdc1dd892
Varun Pius Rodrigues <varunpiusrodrix@gmail.com>
2024-04-28 18:05:30 -0700

    Dagster adding job

--- StockPriceWorkflow/__init__.py
+++ StockPriceWorkflow/__init__.py
@@ -1,9 +1,19 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import (
+    AssetSelection,
+    Definitions,
+    ScheduleDefinition,
+    define_asset_job,
+    load_assets_from_modules
+)
 
 from . import assets
 
 all_assets = load_assets_from_modules([assets])
 
+# Addition: define a job that will materialize the assets
+stock_agg_job = define_asset_job("stock_agg_job", selection=AssetSelection.all())
+
 defs = Definitions(
     assets=all_assets,
+    jobs=[stock_agg_job], 
 )
